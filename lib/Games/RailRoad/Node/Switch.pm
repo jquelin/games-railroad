604af20cdd14f23a3b247017e237d2733acd5338
Jérôme Quelin <jquelin@gmail.com>
2008-04-16 16:46:57 +0200

    shipping our own constructor to make sur _switch is correctly initialized.

--- lib/Games/RailRoad/Node/Switch.pm
+++ lib/Games/RailRoad/Node/Switch.pm
@@ -12,7 +12,14 @@ use strict;
 use warnings;
 
 use base qw{ Games::RailRoad::Node };
+__PACKAGE__->mk_accessors( qw{ _switch } );
 
+sub new {
+    my ($pkg, $opts) = @_;
+    my $self = $pkg->SUPER::new($opts);
+    $self->_switch(0);
+    return $self;
+}
 
 1;
 __END__
@@ -32,6 +39,24 @@ to a given end.
 
 Refer to C<Games::RailRoad::Node> for a description of the various node
 types.
+
+
+
+=head1 METHODS
+
+This class implements the following methods as defined in
+C<Games::RailRoad::Node>:
+
+=over 4
+
+=item * new
+
+
+=back
+
+
+Refer to the documentation in C<Games::RailRoad::Node> to learn more
+about them.
 
 
 
